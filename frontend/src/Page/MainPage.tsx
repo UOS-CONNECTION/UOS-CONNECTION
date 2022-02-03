89f339b645ab4dcf9bf4ae1b02ccc17e808ec980
Lee <43488305+poiu694@users.noreply.github.com>
2022-02-03 15:24:21 +0900

    [Chore] Add sass library

--- frontend/src/Page/MainPage.tsx
+++ frontend/src/Page/MainPage.tsx
@@ -1,12 +1,13 @@
-import Header from '../Component/Header'
-import Layout from '../Component/Layout'
+import Header from '../Component/Header';
+import Layout from '../Component/Layout';
+import '../scss/MainPage.scss';
 
 const MainPage: React.FC = () => {
   return (
     <Layout>
       <Header />
     </Layout>
-  )
-}
+  );
+};
 
 export default MainPage;
