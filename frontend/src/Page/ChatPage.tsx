a963387aad207a16e194e8344d3779bd9220fa77
pasly0920 <inwoo920621@gmail.com>
2022-02-07 23:55:49 +0900

    [Feat] Add Chatpage

---
+++ frontend/src/Page/ChatPage.tsx
@@ -0,0 +1,15 @@
+import Chat from "../Component/Chat";
+import Header from "../Component/Header";
+import Layout from "../Component/Layout";
+import "../scss/ChatPage.scss";
+
+const ChatPage: React.FC = () => {
+  return (
+    <Layout>
+      <Header />
+      <Chat />
+    </Layout>
+  );
+};
+
+export default ChatPage;
