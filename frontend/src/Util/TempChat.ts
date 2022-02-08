39de2e8ef698a91894768160533d617f22b43e94
pasly0920 <inwoo920621@gmail.com>
2022-02-08 11:29:11 +0900

    [Feat] Add Temp Data

---
+++ frontend/src/Util/TempChat.ts
@@ -0,0 +1,50 @@
+export const TempChat = [
+  {
+    id: 1,
+    user: {
+      id: 180,
+      img: null,
+      nickname: "상민",
+    },
+    unwatched: 2,
+    last: "22/02/07/22-20",
+    overview: "인간크롱 뿌뿌뿡",
+    content: [
+      ["에헤이", 1],
+      ["하이하이여", 0],
+      ["님 50만원만 네고 ㄱㄱ", 0],
+      ["허허 50만원 받고 더블로 가 !", 1],
+    ],
+  },
+  {
+    id: 2,
+    user: {
+      id: 160,
+      img: null,
+      nickname: "지함",
+    },
+    unwatched: 0,
+    last: "22/02/05/22-20",
+    overview: "지함 지향적 사고",
+    content: [
+      [" UN village ~~", 1],
+      ["U & me relaxing chilling yeah~~", 0],
+      ["완벽한 모습을 너에게 보이지는 않을 거야~~", 0],
+      ["지금 이 순간 마저 난 모든 것 흘러가는대로 부는 곳으로 바람이~~", 1],
+      ["사랑을 속삭일 때 난 원래 ~~", 0],
+      ["이미 알고 있었다고 해도 ~ 처음 느끼는 기분으로 널 ~~", 1],
+      ["리드할께 lean on~~ me", 1],
+    ],
+  },
+  {
+    id: 3,
+    user: {
+      id: 180,
+      img: null,
+      nickname: "인우",
+    },
+    unwatched: 0,
+    last: "22/02/08/22-20",
+    overview: "책상 breaker",
+  },
+];
