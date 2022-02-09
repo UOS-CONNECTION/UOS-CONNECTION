dff2d506dbe4d4333d8c575a363d713b30fda7d3
pasly0920 <inwoo920621@gmail.com>
2022-02-09 12:29:39 +0900

    [Util] Mock data fix

--- frontend/src/Util/TempChat.ts
+++ frontend/src/Util/TempChat.ts
@@ -10,6 +10,7 @@ export const TempChat = [
     last: "2022/2/7/22/20",
     overview: "인간크롱 뿌뿌뿡",
     content: [
+      // 0 host 1 receiver
       ["에헤이", 1],
       ["하이하이여", 0],
       ["님 50만원만 네고 ㄱㄱ", 0],
@@ -34,6 +35,9 @@ export const TempChat = [
       ["사랑을 속삭일 때 난 원래 ~~", 0],
       ["이미 알고 있었다고 해도 ~ 처음 느끼는 기분으로 널 ~~", 1],
       ["리드할께 lean on~~ me", 1],
+      ["특별한 시간을 보낼 수 있는 자신이 있다는 걸 난 알아~", 0],
+      ["I know that I know that I know that", 0],
+      ["한남동 UN village ~~ 언덕에서 달을 보면", 1],
     ],
   },
   {
@@ -46,5 +50,38 @@ export const TempChat = [
     unwatched: 0,
     last: "2022/2/08/11/20",
     overview: "책상 breaker",
+  },
+  {
+    id: 4,
+    user: {
+      id: 140,
+      img: null,
+      nickname: "케로로",
+    },
+    unwatched: 0,
+    last: "2022/2/08/3/20",
+    overview: "퍼렁별 침략자",
+  },
+  {
+    id: 5,
+    user: {
+      id: 163,
+      img: null,
+      nickname: "코난",
+    },
+    unwatched: 3,
+    last: "2022/2/08/14/20",
+    overview: "몸은 작아졌어도 두뇌는 그래도 포기를 모르는 명탐정",
+  },
+  {
+    id: 6,
+    user: {
+      id: 132,
+      img: null,
+      nickname: "김신",
+    },
+    unwatched: 0,
+    last: "2021/2/28/11/20",
+    overview: "아무르 티그로 캐슬을 박살 내기 위해서 한국에 상륙",
   },
 ];
