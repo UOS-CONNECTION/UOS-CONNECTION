15ad02b33099a01b15471e11849db65fcd1a544d
pasly0920 <inwoo920621@gmail.com>
2022-02-10 12:09:49 +0900

    [Fix] Try fix merge conflict

--- frontend/src/Routes/index.tsx
+++ frontend/src/Routes/index.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MainPage } from '../Page';
-import ChatPage from '../Page/ChatPage';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainPage />} />
-        <Route path='/chat' element={<ChatPage />} />
       </Routes>
     </BrowserRouter>
   );
