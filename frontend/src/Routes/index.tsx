ff9d91ea47c6922449a638f12d94c090a489521e
pasly0920 <inwoo920621@gmail.com>
2022-02-10 12:12:15 +0900

    [Feat] Routing add

--- frontend/src/Routes/index.tsx
+++ frontend/src/Routes/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MainPage } from '../Page';
+import ChatPage from '../Page/ChatPage';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainPage />} />
+        <Route path='/chat' element={<ChatPage />} />
       </Routes>
     </BrowserRouter>
   );
