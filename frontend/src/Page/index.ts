e8adbb47e8aef265db6cff7825dcb7044b2c28e9
Lee <43488305+poiu694@users.noreply.github.com>
2022-02-13 00:29:47 +0900

    [Chore] Add aboutPage Navigation with Routing

--- frontend/src/Page/index.ts
+++ frontend/src/Page/index.ts
@@ -1,3 +1,4 @@
 export { default as MainPage } from './MainPage';
 export { default as DetailPage } from './DetailPage';
 export { default as ChatPage } from './ChatPage';
+export { default as AboutPage } from './AboutPage';
--- frontend/src/Routes/index.tsx
+++ frontend/src/Routes/index.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { DetailPage, MainPage, ChatPage } from '../Page';
+import { DetailPage, MainPage, ChatPage, AboutPage } from '../Page';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainPage />} />
-        <Route path='post/:postId' element={<DetailPage />} />
+        <Route path='/post/:postId' element={<DetailPage />} />
         <Route path='/chat' element={<ChatPage />} />
+        <Route path='/about' element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   );
