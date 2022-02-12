5f763e8f4d6682e462b8f3cf7da33c812ac89e5f
Lee <43488305+poiu694@users.noreply.github.com>
2022-02-13 00:35:34 +0900

    [Feat] Add AboutPage init component

---
+++ frontend/src/Component/About/Description.tsx
@@ -0,0 +1,30 @@
+const Description: React.FC = () => {
+  return (
+    <section className='description-container'>
+      <div className='description-line'>
+        <img src='/img/Talent.png' alt='talent-img' />
+        <div className='description-text-container'>
+          <p className='description-title'>시대이음이란?</p>
+          <p className='description-content'>
+            시대이음은 서울시립대학교 학우 여려분들의 재능을 공유하는
+            자리입니다. 생각보다 가까이 있는 우리들. 우리의 재능을 뽐내
+            여러분들끼리 상부상조할 수 있길 기원합니다.
+          </p>
+        </div>
+      </div>
+      <div className='description-line'>
+        <div className='description-text-container'>
+          <p className='description-title'>시대이음이란?</p>
+          <p className='description-content'>
+            시대이음은 서울시립대학교 학우 여려분들의 재능을 공유하는
+            자리입니다. 생각보다 가까이 있는 우리들. 우리의 재능을 뽐내
+            여러분들끼리 상부상조할 수 있길 기원합니다.
+          </p>
+        </div>
+        <img src='/img/Talent.png' alt='talent-img' />
+      </div>
+    </section>
+  );
+};
+
+export default Description;
