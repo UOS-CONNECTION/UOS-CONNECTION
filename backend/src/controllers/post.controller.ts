40f0e4c74836dcdd5d1884e5b2dd7b6848faade3
Lee <43488305+poiu694@users.noreply.github.com>
2022-02-23 00:53:19 +0900

    [Chore] Add getAllPost routing

--- backend/src/controllers/post.controller.ts
+++ backend/src/controllers/post.controller.ts
@@ -5,9 +5,18 @@ class postController {
   // example user
   async temp(req: Request, res: Response, next: NextFunction) {
     try {
-      const { id } = req.params;
+      const { postId } = req.params;
 
-      return res.json(TempData[Number(id) - 1]);
+      return res.json(TempData[Number(postId) - 1]);
+    } catch (err) {
+      res.sendStatus(400);
+    }
+  }
+
+  async getAllPost(req: Request, res: Response) {
+    try {
+      console.log('send All Data');
+      return res.json(TempData);
     } catch (err) {
       res.sendStatus(400);
     }
