8f7e9d42c35a06a9be39bdd74d99ab937e386858
Lee <43488305+poiu694@users.noreply.github.com>
2022-02-23 00:57:04 +0900

    [Feat] Get data from server with using requestAPI

--- frontend/src/Component/Detail/index.tsx
+++ frontend/src/Component/Detail/index.tsx
@@ -1,6 +1,7 @@
-import { useLayoutEffect, useState } from 'react';
+import { useCallback, useLayoutEffect, useState } from 'react';
+import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { TempData } from '../../Util/TempData';
+import { getOnePost } from '../../Store/Action/postAction';
 import { IData, IInfomation } from '../../Util/Type';
 import DetailCard from './DetailCard';
 import InfomationCard from './InfomationCard';
@@ -35,21 +36,40 @@ const initialPost: IData = {
 };
 
 const Detail: React.FC = () => {
-  const { postId } = useParams();
-
   const [post, setPost] = useState<IData>(initialPost);
   const [basicInfoList, setBasicInfoList] = useState<IInfomation[]>([]);
   const [extraInfoList, setExtraInfoList] = useState<IInfomation[]>([]);
 
+  const dispatch = useDispatch();
+  const { postId } = useParams();
+  const postFromRedux = useSelector(
+    (state: RootStateOrAny) => state.postReducer.post
+  );
+
+  const setPostData = useCallback((postData) => {
+    setPost(postData);
+    setBasicInfoList(postData.User.basic_info);
+    setExtraInfoList(postData.User.extra_info);
+  }, []);
+
+  /**
+   * 리덕스에 상태가 저장이 되어있다면 해당 데이터를, (case => main에서 카드 클릭)
+   * 상태가 저장이 되어있지 않다면 서버로부터 데이터를 저장합니다. (case => 상세페이지에서 새로고침)
+   */
+  const setPostInfo = useCallback(async () => {
+    if (postFromRedux !== undefined) {
+      setPostData(postFromRedux);
+    } else {
+      const res = await dispatch(getOnePost(Number(postId)));
+      if (res.payload.status === 200) {
+        setPostData(res.payload.data);
+      }
+    }
+  }, [dispatch, postFromRedux, postId, setPostData]);
+
   useLayoutEffect(() => {
-    fetch(`http://localhost:8080/api/post/${postId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setPost(data);
-        setBasicInfoList(data.User.basic_info);
-        setExtraInfoList(data.User.extra_info);
-      });
-  }, [postId]);
+    setPostInfo();
+  }, [setPostInfo]);
 
   return (
     <div className='detail-container'>
--- frontend/src/Component/Main/index.tsx
+++ frontend/src/Component/Main/index.tsx
@@ -1,15 +1,31 @@
-import { useState } from 'react';
+import { useCallback, useLayoutEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getAllPost } from '../../Store/Action/postAction';
+import requestAPI from '../../Util/Request';
 import { TempData } from '../../Util/TempData';
 import { IData } from '../../Util/Type';
 import Filter from './Filter';
 import ShareTab from './ShareTab';
 
 const Main: React.FC = () => {
-  const [talentData] = useState<IData[]>(TempData);
+  const [talentData, setTalentData] = useState<IData[]>([]);
   const [locationOption, setLocationOption] = useState<string>('');
   const [tagOption, setTagOption] = useState<[]>([]);
   const [mannerOption, setMannerOption] = useState<string>('');
   const [sortOption, setSortOption] = useState<string>('');
+
+  const dispatch = useDispatch();
+
+  const setInitialPosts = useCallback(async () => {
+    const res = await dispatch(getAllPost());
+    if (res.payload.status === 200) {
+      setTalentData(res.payload.data);
+    }
+  }, [dispatch]);
+
+  useLayoutEffect(() => {
+    setInitialPosts();
+  }, [dispatch, setInitialPosts]);
 
   return (
     <div className='main-container'>
