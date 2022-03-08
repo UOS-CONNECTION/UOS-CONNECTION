2be7b1a34fef914810333556eb37bbf2c7f0c326
pasly0920 <inwoo920621@gmail.com>
2022-03-08 12:41:37 +0900

    [Feat] Add Socket server managing file

---
+++ backend/src/utils/socket.ts
@@ -0,0 +1,22 @@
+import { Server } from 'socket.io';
+
+const io = new Server();
+const Socket = {
+  emit: (event, data) => {
+    console.log(event, data);
+    io.sockets.emit(event, data);
+  },
+};
+
+io.on('connection', (socket) => {
+  console.log(`User ${socket.id} connected`);
+  socket.on('message', (data) => {
+    console.log('client send', data);
+    //database 추가
+    io.emit('upload', data);
+  });
+});
+io.on('disconnection', (socket) => {
+  console.log(`User ${socket.id} disconnected`);
+});
+export { Socket, io };
