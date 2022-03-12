57a14d5b3022733f0e1840bf6163fe02641a6c48
Lee <43488305+poiu694@users.noreply.github.com>
2022-03-12 16:48:24 +0900

    [Feat] init typeorm entities

--- backend/src/entities/user.entity.ts
+++ backend/src/entities/user.entity.ts
@@ -1,8 +1,57 @@
 // type orm user
+import {
+  Column,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  Timestamp,
+} from 'typeorm';
+
+import ChatEntity from './chat.entity';
+import UserInfoEntity from './user.info.entity';
 
 class UserEntity {
-  // @Column()
-  // id!: number;
+  @PrimaryGeneratedColumn()
+  _id!: number;
+
+  @Column('text', { nullable: false })
+  email!: string;
+
+  @Column('text', { nullable: false })
+  password!: string;
+
+  @Column('text', { nullable: false })
+  nickname!: string;
+
+  @Column('text', { nullable: true })
+  image: string;
+
+  // 매너 점수
+  @Column('int', { nullable: true })
+  score: number;
+
+  // 자기자신 소개
+  @Column('text', { nullable: true })
+  desc: string;
+
+  // 신고 당한 횟수
+  @Column('int', { nullable: true })
+  report: number;
+
+  @Column('text', { nullable: true })
+  favorite_post: string;
+
+  @OneToOne(() => UserInfoEntity, (user) => user._id)
+  user_info: UserInfoEntity;
+
+  @OneToMany(() => ChatEntity, (chatRoom) => chatRoom._id)
+  chat_id: ChatEntity;
+
+  @Column('timestamp', { nullable: false })
+  updated_at: Timestamp;
+
+  @Column('timestamp', { nullable: false })
+  created_at: Timestamp;
 }
 
 export default UserEntity;
