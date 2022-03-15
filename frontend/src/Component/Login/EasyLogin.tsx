30b6bcf5f99e4d05440bea641e6a3c756ed4f6b6
Lee <43488305+poiu694@users.noreply.github.com>
2022-03-15 15:40:12 +0900

    [Style] use single quote

--- frontend/src/Component/Login/EasyLogin.tsx
+++ frontend/src/Component/Login/EasyLogin.tsx
@@ -1,19 +1,19 @@
-import { Typography, Avatar } from "@mui/material";
+import { Typography, Avatar } from '@mui/material';
 
 const EasyLogin: React.FC = () => {
   return (
-    <section className="easylogin-container">
-      <Typography variant="h3" className="easylogin-title">
+    <section className='easylogin-container'>
+      <Typography variant='h3' className='easylogin-title'>
         SNS 계정으로 간편 로그인/회원가입
       </Typography>
-      <div className="icons-container">
+      <div className='icons-container'>
         <Avatar
-          className="easylogin-icon"
-          alt="Google"
+          className='easylogin-icon'
+          alt='Google'
           src={`/img/google.ico`}
         />
-        <Avatar className="easylogin-icon" alt="Naver" src={`/img/naver.ico`} />
-        <Avatar className="easylogin-icon" alt="Kakao" src={`/img/kakao.svg`} />
+        <Avatar className='easylogin-icon' alt='Naver' src='/img/naver.ico' />
+        <Avatar className='easylogin-icon' alt='Kakao' src='/img/kakao.svg' />
       </div>
     </section>
   );
--- frontend/src/Component/Login/index.tsx
+++ frontend/src/Component/Login/index.tsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
-import LoginForm from "./LoginForm";
-import LoginOuterLink from "./LoginOuterLink";
-import EasyLogin from "./EasyLogin";
+import LoginForm from './LoginForm';
+import LoginOuterLink from './LoginOuterLink';
+import EasyLogin from './EasyLogin';
 
 const Logo: React.FC = () => {
   return (
-    <div className="logo-container">
-      <img className="logo" alt="Logo" src={`/img/logo_long.png`} />
+    <div className='logo-container'>
+      <img className='logo' alt='Logo' src='/img/logo_long.png' />
     </div>
   );
 };
 
 const Login: React.FC = () => {
   return (
-    <div className="login-container">
-      <div className="login-container-box">
+    <div className='login-container'>
+      <div className='login-container-box'>
         <Logo />
         <LoginForm />
         <LoginOuterLink />
