d2598faf0767bb09da1b4772e5c31ff95ec4944e
pasly0920 <inwoo920621@gmail.com>
2022-03-17 00:04:31 +0900

    [Fix] Delete banner in About Page change margin little

--- frontend/src/Page/AboutPage.tsx
+++ frontend/src/Page/AboutPage.tsx
@@ -1,14 +1,12 @@
 import Header from '../Component/Header';
 import Layout from '../Component/Layout';
 import About from '../Component/About';
-import Banner from '../Component/Banner';
 import '../scss/AboutPage.scss';
 
 const MainPage: React.FC = () => {
   return (
     <Layout>
       <Header />
-      <Banner />
       <About />
     </Layout>
   );
