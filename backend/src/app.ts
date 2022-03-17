8a65b37002687e82401f2e66203ce47b28cd6314
Lee <43488305+poiu694@users.noreply.github.com>
2022-03-17 16:40:06 +0900

    [Chore] Observer localLogin with crypto, passport

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
-import express from "express";
-import cors from "cors";
-import * as http from "http";
+import cors from 'cors';
+import * as http from 'http';
+import express from 'express';
+import passport from 'passport';
+import session from 'express-session';
+const FileStore = require('session-file-store')(session);
 
-import DBConfig from "./db";
-import SocketAPI from "./utils/socket";
-import rootRouter from "./routes";
+import DBConfig from './db';
+import rootRouter from './routes';
+import SocketAPI from './utils/socket';
+import passportAPI from './utils/passport';
 
 export default class App {
   app: express.Application;
@@ -13,28 +17,38 @@ export default class App {
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT || "8080";
+    this.port = process.env.PORT || '8080';
     this.config();
     this.middleware();
     this.route();
   }
 
   private config() {
-    this.app.use(express.json()); // json parsing
     this.app.use(express.urlencoded({ extended: false })); // body parsing
+    this.app.use(express.json()); // json parsing
+    this.app.use(passport.initialize());
+    this.app.use(
+      session({
+        secret: 'asadlfkj!@#!@#dfgasdg',
+        resave: false,
+        saveUninitialized: true,
+        store: new FileStore(),
+      })
+    );
+    passportAPI.observerLocalLogin();
     DBConfig.connectionDB();
   }
 
   private middleware() {
     const corsOption = {
-      origin: process.env.FRONTEND_URL || "http://localhost:3000",
+      origin: process.env.FRONTEND_URL || 'http://localhost:3000',
       credentials: true,
     };
     this.app.use(cors(corsOption));
   }
 
   private route() {
-    this.app.use("/api", rootRouter);
+    this.app.use('/api', rootRouter);
   }
 
   listen() {
