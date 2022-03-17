c2831f65c28679178ff70d22f5b7e201bc330898
JihamSong <90690578+JihamSong@users.noreply.github.com>
2022-03-17 11:21:36 +0900

    [Fix] Delete next parameter

--- backend/src/controllers/login.controller.ts
+++ backend/src/controllers/login.controller.ts
@@ -1,21 +1,21 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
 
-var passport = require("passport");
+var passport = require('passport');
 
 class loginController {
-  async temp(req: Request, res: Response, next: NextFunction) {
+  async temp(req: Request, res: Response) {
     try {
-      res.render("login");
+      res.render('login');
     } catch (err) {
       res.sendStatus(400);
     }
   }
 
-  async login(req: Request, res: Response, next: NextFunction) {
+  async login(req: Request, res: Response) {
     try {
-      passport.authenticate("local", {
-        successRedirect: "/",
-        failureRedirect: "/login",
+      passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
       });
     } catch (err) {
       res.sendStatus(400);
