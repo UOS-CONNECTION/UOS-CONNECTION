debbc4a4123b903cd42a396503b78f1890c903a1
JihamSong <jeeham0226@gmail.com>
2022-03-20 23:12:41 +0900

    [Feat] Create kakao-strategy connected to easy-login

--- frontend/src/Component/Login/EasyLogin.tsx
+++ frontend/src/Component/Login/EasyLogin.tsx
@@ -1,6 +1,18 @@
 import { Typography, Avatar } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const EasyLogin: React.FC = () => {
+  const navigate = useNavigate();
+  const handleGoogleClick = () => {
+    navigate('/auth/google');
+  };
+  const handleNaverClick = () => {
+    navigate('/auth/naver');
+  };
+  const handleKakaoClick = () => {
+    navigate('/auth/kakao');
+  };
+
   return (
     <section className='easylogin-container'>
       <Typography variant='h3' className='easylogin-title'>
@@ -11,9 +23,20 @@ const EasyLogin: React.FC = () => {
           className='easylogin-icon'
           alt='Google'
           src={`/img/google.ico`}
+          onClick={handleGoogleClick}
         />
-        <Avatar className='easylogin-icon' alt='Naver' src='/img/naver.ico' />
-        <Avatar className='easylogin-icon' alt='Kakao' src='/img/kakao.svg' />
+        <Avatar
+          className='easylogin-icon'
+          alt='Naver'
+          src='/img/naver.ico'
+          onClick={handleNaverClick}
+        />
+        <Avatar
+          className='easylogin-icon'
+          alt='Kakao'
+          src='/img/kakao.svg'
+          onClick={handleKakaoClick}
+        />
       </div>
     </section>
   );
