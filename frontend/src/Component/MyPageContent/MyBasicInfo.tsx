2f3ee70d2a0f3f8a134b9f6d924f04af29013aac
pasly0920 <inwoo920621@gmail.com>
2022-04-11 22:28:23 +0900

    [Feat] Little Change for Modal & change name & dsc

--- frontend/src/Component/MyPageContent/MyBasicInfo.tsx
+++ frontend/src/Component/MyPageContent/MyBasicInfo.tsx
@@ -1,26 +1,83 @@
 import { Avatar, Box, TextField, Typography, Button } from '@mui/material';
+import { useState } from 'react';
 import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 
 const MyBasicInfo: React.FC = () => {
+  const [isNameClicked, setIsNameClicked] = useState(false);
+  const [isDscClicked, setIsDscClicked] = useState(false);
+  const [isImgClicked, setIsImgClicked] = useState(false);
+
+  const imgClickHandler = () => {
+    setIsImgClicked((prev) => !prev);
+  };
+  const nameClickHandler = () => {
+    setIsNameClicked((prev) => !prev);
+  };
+  const DSCHandler = () => {
+    setIsDscClicked((prev) => !prev);
+  };
+
+  const handleImgModalOff = (event: any) => {
+    const clicked = event.target.closest('.img-notice');
+
+    if (clicked) return;
+    else setIsImgClicked(false);
+  };
+
   return (
     <Box className='basic-info-container'>
       <Box className='user-img-container'>
-        <Avatar alt='user img' src='' className='info-user-img' />
+        <Avatar
+          alt='user img'
+          src=''
+          className='info-user-img'
+          onClick={imgClickHandler}
+        />
+        <SettingsOutlinedIcon
+          className='user-img-edit-btn'
+          onClick={imgClickHandler}
+        />
       </Box>
       <Box className='basic-info'>
         <Box className='user-name-container'>
-          <Typography variant='h5' className='user-name'>
-            지빈
-          </Typography>
-          <ModeEditOutlineOutlinedIcon className='edit-btn' />
+          {isNameClicked ? (
+            <TextField
+              variant='standard'
+              placeholder='지함'
+              size='small'
+              className='user-name-input'
+            />
+          ) : (
+            <Typography variant='h5' className='user-name'>
+              지함
+            </Typography>
+          )}
+
+          <ModeEditOutlineOutlinedIcon
+            className='edit-btn'
+            onClick={nameClickHandler}
+          />
         </Box>
         <Box className='description-container'>
-          <TextField fullWidth multiline />
-          <Button variant='outlined' className='edit-btn'>
+          {isDscClicked ? (
+            <TextField
+              fullWidth
+              multiline
+              maxRows={3}
+              placeholder='This is self-description.'
+            />
+          ) : (
+            <Typography className='user-dsc'>
+              This is self-description.~~~~~~~~~~~~~~~
+            </Typography>
+          )}
+          <Button variant='outlined' className='edit-btn' onClick={DSCHandler}>
             수정
           </Button>
         </Box>
       </Box>
+      {/* modal */}
     </Box>
   );
 };
