c42df6462d3a57ddca11b855d4cb64db8b1e5a08
Lee <43488305+poiu694@users.noreply.github.com>
2022-05-14 17:47:37 +0900

    [Refactor] userLogin 기능 도입

--- frontend/src/Store/Action/userAction.ts
+++ frontend/src/Store/Action/userAction.ts
@@ -1,7 +1,9 @@
 import { LOG_IN_REQUEST } from '../type';
+import { LoginRequeset } from '../../Types/UserType';
+import requestAPI from '../../Util/Request';
 
-export const loginUser = async () => {
-  const res = await 2;
+export const loginUser = async ({ email, password }: LoginRequeset) => {
+  const res = await requestAPI.post('/api/user/login', { email, password });
 
   return {
     type: LOG_IN_REQUEST,
