1e9aa72c6b33d21ce0e321161d226fcfd8e799b2
Lee <43488305+poiu694@users.noreply.github.com>
2022-05-16 15:49:17 +0900

    [Refactor] 실행 오류될만한 부분 제거

--- frontend/src/Page/KakaoCallbackPage.tsx
+++ frontend/src/Page/KakaoCallbackPage.tsx
@@ -1,7 +1,11 @@
 import Layout from '../Component/Layout';
 
 const KakaoCallbackPage: React.FC = () => {
-  return <Layout><></></Layout>;
+  return (
+    <Layout>
+      <span>callback</span>
+    </Layout>
+  );
 };
 
 export default KakaoCallbackPage;
